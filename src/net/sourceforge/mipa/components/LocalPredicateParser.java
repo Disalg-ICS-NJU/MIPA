206e2ca5ca63d4edf794d9cfed2303038461d198
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-07-03 09:11:53 +0000

    add resource manager files.

---
+++ src/net/sourceforge/mipa/components/LocalPredicateParser.java
@@ -0,0 +1,55 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.components;
+
+import net.sourceforge.mipa.predicatedetection.PredicateType;
+
+import org.w3c.dom.Document;
+
+/**
+ * local predicate parser module.
+ * 
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class LocalPredicateParser {
+
+    /**
+     * parse local predicate from <code>Document</code>.
+     * 
+     * @param predicate
+     *            a document
+     * @param coordinateID
+     *            a String, usually is applicationName
+     * @param type
+     *            predicate type
+     */
+    public void parseLocalPredicate(Document predicate, String coordinateID,
+                                    PredicateType type) {
+        // TODO parse local predicate
+        System.out.println("parsing local preidcate...");
+
+        /*
+         * NodeList elements = predicate.getElementsByTagName("LP"); for(int i =
+         * 0; i < elements.getLength(); i++) {
+         * 
+         * }
+         */
+    }
+}
