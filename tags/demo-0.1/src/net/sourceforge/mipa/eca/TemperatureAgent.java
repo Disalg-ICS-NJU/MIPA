0188a8f84120bef3d7961ff5c4870d6b7caa866c
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-07-07 07:33:16 +0000

    tags demo-0.1 version.

---
+++ tags/demo-0.1/src/net/sourceforge/mipa/eca/TemperatureAgent.java
@@ -0,0 +1,91 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.eca;
+
+import static config.Debug.DEBUG;
+import net.sourceforge.mipa.eca.sensor.Sensor;
+import net.sourceforge.mipa.eca.sensor.Temperature;
+
+/**
+ * Temperature sensor agent.
+ *
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class TemperatureAgent implements SensorAgent {
+    
+    /** data source of ECA mechanism. */
+    private DataSource dataSource;
+    
+    /** the sensor which sensor agent manages */
+    private Sensor sensor;
+    
+    /** sensor agent's name */
+    private String name;
+    
+    /** sensor agent's value type */
+    private String valueType;
+    
+    
+    public TemperatureAgent(DataSource source, String name, String valueType) {
+        this.dataSource = source;
+        this.name = name;
+        this.valueType = valueType;
+        sensor = new Temperature();
+    }
+
+    @Override
+    public String getValueType() {
+        return valueType;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public void generateData() {
+        
+    }
+
+    @Override
+    public String getData() {
+        
+        return null;
+    }
+
+    @Override
+    public void run() {
+        //Thread.yield();
+        try {
+            while(true) {
+                Thread.sleep(1000);
+                String[] values = {sensor.getData()};
+                if(DEBUG) {
+                    System.out.println("temperature now is " + values[0]);
+                }
+                dataSource.update(this.name, values);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ tags/demo-0.1/src/net/sourceforge/mipa/predicatedetection/LocalPredicateParser.java
@@ -0,0 +1,108 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.predicatedetection;
+
+import static config.Debug.DEBUG;
+import net.sourceforge.mipa.components.ContextMapping;
+import net.sourceforge.mipa.components.MIPAResource;
+import net.sourceforge.mipa.eca.ECAManager;
+import net.sourceforge.mipa.naming.Naming;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * local predicate parser module.
+ * 
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class LocalPredicateParser {
+
+    /** context mapping */
+    private ContextMapping contextMapping;
+
+    public LocalPredicateParser(ContextMapping contextMapping) {
+        this.contextMapping = contextMapping;
+    }
+
+    /**
+     * parse local predicate from <code>Document</code>.
+     * 
+     * @param predicate
+     *            a document
+     * @param coordinateID
+     *            a String, usually is applicationName
+     * @param type
+     *            predicate type
+     */
+    public void parseLocalPredicate(Document predicate, String coordinateID,
+                                    PredicateType type) {
+        // TODO parse local predicate
+        if (DEBUG) {
+            System.out.println("\tparsing local preidcate...");
+
+            LocalPredicate localPredicate = new LocalPredicate();
+            localPredicate.setName("temperature");
+            localPredicate.setOperator("great-than");
+            localPredicate.setValue("40");
+
+            try {
+                String ecaManagerId = contextMapping
+                                                    .getEntityId(localPredicate
+                                                                               .getName());
+                localPredicate
+                              .setValueType(contextMapping
+                                                          .getValueType(localPredicate
+                                                                                      .getName()));
+                // FIXME put lookup to a method
+                Naming server = (Naming) java.rmi.Naming
+                                                        .lookup(MIPAResource
+                                                                            .getNamingAddress()
+                                                                + "Naming");
+                ECAManager ecaManager = (ECAManager) server
+                                                           .lookup(ecaManagerId);
+                System.out.println("find eca manager successfully.");
+                System.out.println(ecaManagerId);
+                ecaManager.registerLocalPredicate(localPredicate, coordinateID,
+                                                  type);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        /*
+        NodeList elements = predicate.getElementsByTagName("LP");
+        if(elements != null) { 
+            for(int i = 0; i < elements.getLength(); i++) { 
+                Node localPredicate = elements.item(i);
+                Node formula = localPredicate.getChildNodes().item(0);
+                
+            }
+         }
+*/
+        /*
+         * NodeList elements = predicate.getElementsByTagName("LP"); for(int i =
+         * 0; i < elements.getLength(); i++) {
+         * 
+         * }
+         */
+    }
+}
