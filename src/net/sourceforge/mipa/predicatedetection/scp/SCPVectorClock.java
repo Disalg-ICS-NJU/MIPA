88dd51b39551e50b994a9d2d7fc66a6488b9162b
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-07-09 07:29:34 +0000

    add SCP vector clock.

---
+++ src/net/sourceforge/mipa/predicatedetection/scp/SCPVectorClock.java
@@ -0,0 +1,61 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.predicatedetection.scp;
+
+import net.sourceforge.mipa.predicatedetection.VectorClock;
+
+/**
+ * 
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class SCPVectorClock extends VectorClock {
+
+    private static final long serialVersionUID = 3657720011833321444L;
+
+    /**
+     * @param number
+     *            the number of normal processes
+     */
+    public SCPVectorClock(int number) {
+        super(number);
+    }
+
+    /**
+     * @param clock
+     *            vector clock
+     */
+    public SCPVectorClock(VectorClock clock) {
+        super(clock);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.sourceforge.mipa.predicatedetection.VectorClock#update(net.sourceforge
+     * .mipa.predicatedetection.VectorClock)
+     */
+    @Override
+    public void update(VectorClock timestamp) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
