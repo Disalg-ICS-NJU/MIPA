20cee0bdbf45d06316812a9938f63a5f6afe6d66
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-07-16 14:15:47 +0000

    tag release 0.3.

---
+++ tags/0.3/src/net/sourceforge/mipa/components/Message.java
@@ -0,0 +1,157 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.components;
+
+import java.io.Serializable;
+
+import net.sourceforge.mipa.predicatedetection.VectorClock;
+import net.sourceforge.mipa.predicatedetection.scp.SCPMessageContent;
+
+/**
+ * message class.
+ * 
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = -8694974992774436096L;
+    
+    /** message type */
+    private MessageType type;
+    
+    /** message sender */
+    private String senderID;
+    
+    /** message receiver */
+    private String receiverID;
+    
+    /** when the message arrives at dispatcher */
+    private long reachTime;
+    
+    /** dispatch time set by dispatcher */
+    private long dispatchTime;
+    
+    /** vector clock of message */
+    private VectorClock timestamp;
+    
+    /** SCP message content */
+    private SCPMessageContent scpMessageContent = null;
+    
+    //TODO other predicate message content types go here.
+    
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the type
+     */
+    public MessageType getType() {
+        return type;
+    }
+
+    /**
+     * @param senderID the senderID to set
+     */
+    public void setSenderID(String senderID) {
+        this.senderID = senderID;
+    }
+
+    /**
+     * @return the senderID
+     */
+    public String getSenderID() {
+        return senderID;
+    }
+
+    /**
+     * @param receiverID the receiverID to set
+     */
+    public void setReceiverID(String receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    /**
+     * @return the receiverID
+     */
+    public String getReceiverID() {
+        return receiverID;
+    }
+
+    /**
+     * @param reachTime the reachTime to set
+     */
+    public void setReachTime(long reachTime) {
+        this.reachTime = reachTime;
+    }
+
+    /**
+     * @return the reachTime
+     */
+    public long getReachTime() {
+        return reachTime;
+    }
+
+    /**
+     * @param dispatchTime the dispatchTime to set
+     */
+    public void setDispatchTime(long dispatchTime) {
+        this.dispatchTime = dispatchTime;
+    }
+
+    /**
+     * @return the dispatchTime
+     */
+    public long getDispatchTime() {
+        return dispatchTime;
+    }
+
+    /**
+     * @param timestamp the timestamp to set
+     */
+    public void setTimestamp(VectorClock timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public VectorClock getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @param scpMessageContent the scpMessageContent to set
+     */
+    public void setScpMessageContent(SCPMessageContent scpMessageContent) {
+        this.scpMessageContent = scpMessageContent;
+    }
+
+    /**
+     * @return the scpMessageContent
+     */
+    public SCPMessageContent getScpMessageContent() {
+        return scpMessageContent;
+    }
+}
---
+++ tags/0.3/src/net/sourceforge/mipa/eca/ECAManagerImp.java
@@ -0,0 +1,176 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.eca;
+
+import static config.Debug.DEBUG;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+import net.sourceforge.mipa.components.ContextRegister;
+import net.sourceforge.mipa.components.Coordinator;
+import net.sourceforge.mipa.components.MIPAResource;
+import net.sourceforge.mipa.naming.Catalog;
+import net.sourceforge.mipa.naming.IDManager;
+import net.sourceforge.mipa.naming.Naming;
+import net.sourceforge.mipa.predicatedetection.LocalPredicate;
+import net.sourceforge.mipa.predicatedetection.NormalProcess;
+import net.sourceforge.mipa.predicatedetection.PredicateType;
+import net.sourceforge.mipa.predicatedetection.scp.SCPNormalProcess;
+
+/**
+ * 
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class ECAManagerImp implements ECAManager {
+
+    /** name of ECAManager */
+    private String ecaManagerName;
+
+    private ContextRegister contextRegister;
+
+    private DataSource dataSource;
+
+    public ECAManagerImp(ContextRegister contextRegister,
+                         DataSource dataSource, String ecaManagerName) {
+        this.setContextRegister(contextRegister);
+        this.ecaManagerName = ecaManagerName;
+        /*
+        try {
+            Naming server = MIPAResource.getNamingServer();
+            this.dataSource = (DataSource) server.lookup(dataSourceId);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        */
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * @param ecaManagerName
+     *            the ecaManagerName to set
+     */
+    public void setECAManagerName(String ecaManagerName) {
+        this.ecaManagerName = ecaManagerName;
+    }
+
+    /**
+     * @return the ecaManagerName
+     */
+    public String getECAManagerName() {
+        return this.ecaManagerName;
+    }
+
+    /**
+     * @param contextRegister
+     *            the contextRegister to set
+     */
+    public void setContextRegister(ContextRegister contextRegister) {
+        this.contextRegister = contextRegister;
+    }
+
+    /**
+     * @return the contextRegister
+     */
+    public ContextRegister getContextRegister() {
+        return contextRegister;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.sourceforge.mipa.eca.ECAManager#registerLocalPredicate(net.sourceforge
+     * .mipa.predicatedetection.LocalPredicate, java.lang.String)
+     */
+    @Override
+    public void registerLocalPredicate(LocalPredicate localPredicate,
+                                       String groupId, PredicateType type)
+                                                                          throws RemoteException {
+        // new listener and condition
+        // TODO listener should be Normal process and remove lookup to construction.
+        
+        try {
+            Naming server = MIPAResource.getNamingServer();
+        
+            IDManager idManger = MIPAResource.getIDManager();
+            
+            Coordinator coordinator = MIPAResource.getCoordinator();
+            
+            String npName = idManger.getID(Catalog.NormalProcess);
+            
+            if(DEBUG) {
+                System.out.println("Get normal process name: " + npName);
+            }
+        
+            SCPNormalProcess np = new SCPNormalProcess(npName);
+            NormalProcess npStub = (NormalProcess) UnicastRemoteObject.exportObject(np, 0);
+            
+            server.bind(npName, npStub);
+            coordinator.normalProcessFinished(groupId, npName);
+        
+            if(DEBUG) {
+                System.out.println("before binding condition...");
+            }
+            Condition everything = new EmptyCondition(np, localPredicate);
+            
+            if(DEBUG) {
+                System.out.println("after binding condition...");
+            }
+            // attaching condition to data source.
+
+            if (DEBUG) {
+                System.out.println("local predicate name is "
+                                   + localPredicate.getName());
+            }
+
+            // FIXME should attach local predicate related events, get from atoms.
+            dataSource.attach(everything, localPredicate.getName());
+            
+            if(DEBUG) {
+                System.out.println("binding condition to data source successful.");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * registers local resource to resource manager.
+     * 
+     * @param resources
+     *            local resources
+     */
+    public void registerResources(ArrayList<SensorAgent> resources) {
+        // TODO auto scan local resource
+
+        try {
+            for (int i = 0; i < resources.size(); i++) {
+                SensorAgent resource = resources.get(i);
+                contextRegister.registerResource(resource.getName(),
+                                                 resource.getValueType(),
+                                                 ecaManagerName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
