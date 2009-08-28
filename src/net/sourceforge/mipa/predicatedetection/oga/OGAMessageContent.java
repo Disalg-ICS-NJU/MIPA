baed06f1aa9a28b52ccbf5649ca9ad3e3fc0fb86
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-08-28 08:41:44 +0000

    complish OGA normal process algorithm.

---
+++ src/net/sourceforge/mipa/predicatedetection/oga/OGAMessageContent.java
@@ -0,0 +1,40 @@
+/* 
+ * MIPA - Middleware Infrastructure for Predicate detection in Asynchronous 
+ * environments
+ * 
+ * Copyright (C) 2009 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the term of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.mipa.predicatedetection.oga;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jianping Yu <jianp.yue@gmail.com>
+ */
+public class OGAMessageContent implements Serializable {
+
+    private static final long serialVersionUID = -2412107813802433533L;
+
+    private OGAVectorClock lo;
+    
+    private OGAVectorClock hi;
+    
+    public OGAMessageContent(OGAVectorClock lo, OGAVectorClock hi) {
+        this.lo = lo;
+        this.hi = hi;
+    }
+}
