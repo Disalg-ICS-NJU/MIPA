e91f06f7d0ed00f771a29c7e4a91a2b707d80510
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-11-22 09:15:39 +0000

    For some reasons, remove tag 1.3.0. It will tag later.

--- tags/1.3.0/bin/linux/initialize.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-MIPA_HOME="."
-cd ../..
-java -cp "$MIPA_HOME/build/classes:$CLASSPATH" net.sourceforge.mipa.Initialize
--- tags/1.3.0/bin/windows/application.bat
+++
@@ -1,3 +0,0 @@
-set MIPA_HOME=.
-cd ..\..
-java -cp "%MIPA_HOME%\build\classes\;%CLASSPATH%" net.sourceforge.mipa.Application predicate_scp.xml 1
