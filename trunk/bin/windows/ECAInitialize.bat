5f77c9a9f0cb2a6ea7ad359d6790f47c0ee098e8
jianp.yue <jianp.yue@167382cc-5a47-11de-8832-3f4c773c73ae>
2009-12-08 08:58:05 +0000

    correct windows startup scripts.

--- trunk/bin/windows/ECAInitialize.bat
+++ trunk/bin/windows/ECAInitialize.bat
@@ -1,4 +1,4 @@
 set MIPA_HOME=.
-set MIPA_CLASSPATH=%MIPA_HOME%\build\classes\:%CLASSPATH%
+set MIPA_CLASSPATH=%MIPA_HOME%\build\classes\;%CLASSPATH%
 cd ..\..
 java -cp "%MIPA_CLASSPATH%" net.sourceforge.mipa.ECAInitialize
