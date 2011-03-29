351dd9d616fdd998458912db2405b4af1bedf044
jpyu.mail <jpyu.mail@167382cc-5a47-11de-8832-3f4c773c73ae>
2011-03-29 02:49:18 +0000

    commit preprocessing code.

---
+++ src/net/sourceforge/mipa/eca/preprocessing/PipelineBeginUnit.java
@@ -0,0 +1,11 @@
+package net.sourceforge.mipa.eca.preprocessing;
+
+public class PipelineBeginUnit extends PipelineUnit {
+	public PipelineBeginUnit() {
+		
+	}
+	
+	public void update(String[] data) {
+		
+	}
+}
